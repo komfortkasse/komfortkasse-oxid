406d278b2791ca7a7108bbd7e4757441d0ea6de5
machajdik <manuel.machajdik@gmail.com>
2017-03-11 18:58:46 +0100

    v1.7.4

--- ltc/komfortkasse/models/komfortkasseoxorder.php
+++ ltc/komfortkasse/models/komfortkasseoxorder.php
@@ -3,11 +3,13 @@ class komfortkasseOxOrder extends komfortkasseOxOrder_parent
 {
 	/** @overload */
 	public function finalizeOrder( oxBasket $oBasket, $oUser, $blRecalculatingOrder = false) {
-		if (parent::finalizeOrder($oBasket, $oUser, $blRecalculatingOrder) == oxOrder::ORDER_STATE_OK) {
+	    $ret = parent::finalizeOrder($oBasket, $oUser, $blRecalculatingOrder);
+		if ($ret == oxOrder::ORDER_STATE_OK) {
 			$k = new Komfortkasse();
 			$k->notifyorder($this->oxorder__oxordernr->value);
-		}		
+		}
+		return $ret;
 	}
 
-	
+
 }
